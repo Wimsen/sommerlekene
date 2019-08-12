40efffb65a0f1bbc66bcf50f09cda893f8941116
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2019-08-12 23:36:28 +0200

    Remove useless login button

--- frontend/src/containers/appbar.js
+++ frontend/src/containers/appbar.js
@@ -5,7 +5,6 @@ import { connect } from "react-redux";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/Drawer";
@@ -59,7 +58,6 @@ class ButtonAppBar extends Component {
                         <Typography color="inherit" onClick={this.changeRoute("/")}>
                             Sommerlekene
                         </Typography>
-                        <Button color="inherit">Login</Button>
                     </Toolbar>
                 </AppBar>
             </div>
